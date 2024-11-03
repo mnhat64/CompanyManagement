e6882a9e148b1118f547768194f272b69e6829a2
Minh Nhat Phan <69678769+mnhat64@users.noreply.github.com>
2024-11-03 21:31:26 +0100

    DB initiated

---
+++ src/main/java/com/minh/jpa/JpaApplication.java
@@ -0,0 +1,13 @@
+package com.minh.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minh/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
